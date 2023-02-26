86f2a201583ad6e62cd983ba6fdcbafe7729cadf
unknown <s_ekimyan@mail.ru>
2023-02-26 12:00:57 +0300

    домашнее задание

--- homework3/Program.cs
+++ homework3/Program.cs
@@ -1,8 +1,4 @@
 ﻿// Задача 19
-// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-//  Через строку решать нельзя.
-// 14212 -> нет
-// 12821 -> да
 // try
 // {
 //     Console.Write("Введите число: ");
@@ -29,11 +25,8 @@
 // {
 //     Console.WriteLine("Были введены некорректные данные");
 // }
-// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-// A (3,6,8); B (2,1,-7), -> 15.84
-// A (7,-5, 0); B (1,-1,9) -> 11.53
+//Задача 21
 // Console.WriteLine("Введите начение X1");
-// int X1 = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine("Введите начение Y1");
 // int Y1 = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine("Введите начение Z1");
@@ -46,9 +39,8 @@
 // int Z2 = Convert.ToInt32(Console.ReadLine());
 // double res = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2)+ Math.Pow(Z2 - Z1, 2));
 // Console.WriteLine(Math.Round(res, 2));
-// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-// 3 -> 1, 8, 27
-// 5 -> 1, 8, 27, 64, 125
+
+//Задача 23
 Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
 string res = String.Empty;
---
+++ homework4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 25
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// double res = Math.Pow(num1, num2);
+// Console.WriteLine($"{num1},{num2} -> {res}");
+
+// Задача 27
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int k =num; 
+// int res = 0;
+// while (num>0)
+// {
+//     res = res + num%10;
+//     num = num/10;
+// }
+// Console.WriteLine($"{k} -> {res}");
+
+// Задача 29
+void ArrayFull(int [] array){
+    for (int k = 0; k< array.Length; k++)
+        array[k] = new Random().Next(1,100);
+}
+void PrintArray(int[] array){
+    Console.WriteLine($"{string.Join(", ", array)} -> [{string.Join(", ", array)}]");
+}
+int [] array1 = new int[8];
+ArrayFull(array1);
+PrintArray(array1);
